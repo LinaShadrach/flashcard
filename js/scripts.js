09021b699e30dbd414714a02bd1f94e2bd56ea39
Jonathan Thom and Lina Shadrach <lina@>
2016-10-18 13:39:33 -0700

    Add definitions, javascript to make elements appear and disappear.

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,98 @@
 $(function() {
   $(".term").addClass("appear");
-  $ var clicked = function(term, definition){
-    $("#t1 .term").click(function() {
-      $("#t1 .definition").addClass("appear");
-      $("#t1 .term").removeClass("appear");
 
+  $("#t1").click(function() {
+    $("#t1 .definition").toggle();
+    $("#t1 .term").toggle();
     });
 
-    $("#t1 .definition").click(function() {
-      $("#t1 .term").addClass("appear");
-      $("#t1 .definition").removeClass("appear");
+  $("#t2").click(function() {
+    $("#t2 .definition").toggle();
+    $("#t2 .term").toggle();
     });
-  };
+
+  $("#t3").click(function() {
+    $("#t3 .definition").toggle();
+    $("#t3 .term").toggle();
+    });
+
+  $("#t4").click(function() {
+    $("#t4 .definition").toggle();
+    $("#t4 .term").toggle();
+    });
+
+  $("#t5").click(function() {
+    $("#t5 .definition").toggle();
+    $("#t5 .term").toggle();
+    });
+
+  $("#t6").click(function() {
+    $("#t6 .definition").toggle();
+    $("#t6 .term").toggle();
+    });
+
+  $("#t7").click(function() {
+    $("#t7 .definition").toggle();
+    $("#t7 .term").toggle();
+    });
+
+  $("#t8").click(function() {
+    $("#t8 .definition").toggle();
+    $("#t8 .term").toggle();
+    });
+
+  $("#t9").click(function() {
+    $("#t9 .definition").toggle();
+    $("#t9 .term").toggle();
+    });
+
+  $("#t10").click(function() {
+    $("#t10 .definition").toggle();
+    $("#t10 .term").toggle();
+    });
+
+  $("#t11").click(function() {
+    $("#t11 .definition").toggle();
+    $("#t11 .term").toggle();
+    });
+
+  $("#t12").click(function() {
+    $("#t12 .definition").toggle();
+    $("#t12 .term").toggle();
+    });
+
+  $("#t13").click(function() {
+    $("#t13 .definition").toggle();
+    $("#t13 .term").toggle();
+    });
+
+  $("#t14").click(function() {
+    $("#t14 .definition").toggle();
+    $("#t14 .term").toggle();
+    });
+
+  $("#t15").click(function() {
+    $("#t15 .definition").toggle();
+    $("#t15 .term").toggle();
+    });
+
+  $("#t16").click(function() {
+    $("#t16 .definition").toggle();
+    $("#t16 .term").toggle();
+    });
+
+  $("#t17").click(function() {
+    $("#t17 .definition").toggle();
+    $("#t17 .term").toggle();
+    });
+
+  $("#t18").click(function() {
+    $("#t18 .definition").toggle();
+    $("#t18 .term").toggle();
+    });
+
+  $("#t19").click(function() {
+    $("#t19 .definition").toggle();
+    $("#t19 .term").toggle();
+    });
 });
